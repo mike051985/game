bed07dac3022f27c5b071b02763ff785fe9c127d
Michelet Saint-Preux <michelet2000@gmail.com>
2022-01-23 08:23:16 +0800

    Edit index.html index.js style.css

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-const computerDisplay = document.getElementById('computer-play')
-const userDisplay = document.getElementById('user-play')
-const resultDisplay = document.getElementById('result')
-const possiblePlays = document.querySelectorAll('button')
-let userPlay
-let computerPlay
-let result
+const computerDisplay = document.getElementById('computer-play');
+const userDisplay = document.getElementById('user-play');
+const resultDisplay = document.getElementById('result');
+const possiblePlays = document.querySelectorAll('button');
+let userPlay;
+let computerPlay;
+let result;
 
 possiblePlays.forEach(possiblePlay => possiblePlay.addEventListener('click', (e) => {
-    userPlay = e.target.id
-    userDisplay.textContent = userPlay
-    generateComputerPlay()
-    getResult() 
-}))
+    userPlay = e.target.id;
+    userDisplay.textContent = userPlay;
+    generateComputerPlay();
+    getResult(); 
+}));
 
 function generateComputerPlay() {
-    const randomNumber = Math.floor(Math.random() * possiblePlays.length) 
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
 
     if(randomNumber === 1) {
-        computerPlay = 'rock'
+        computerPlay = 'rock';
     }
     if(randomNumber === 2) {
-        computerPlay = 'scissors'
+        computerPlay = 'scissors';
     }
-    if(randomNumber === 1) {
-        computerPlay = 'paper'
+    if(randomNumber === 3) {
+        computerPlay = 'paper';
     }
-    computerDisplay.textContent = computerPlay
+    computerDisplay.textContent = computerPlay;
 }
 
 function getResult() {
     if (computerPlay === userPlay) {
-        result = 'It is a draw!'
+        result = 'It is a draw!';
     }
     if (computerPlay === 'rock' && userPlay === 'paper') {
-        result = 'You win!'
+        result = 'You win!';
     }
     if (computerPlay === 'rock' && userPlay === 'scissors') {
-        result = 'You lose'
+        result = 'You lose';
     }
     if (computerPlay === 'paper' && userPlay === 'scissors') {
-        result = 'You win!'
+        result = 'You win!';
     }
     if (computerPlay === 'paper' && userPlay === 'rock') {
-        result = 'You lose!'
+        result = 'You lose!';
     }
     if (computerPlay === 'scissors' && userPlay === 'rock') {
-        result = 'You win!'
+        result = 'You win!';
     }
     if (computerPlay === 'scissors' && userPlay === 'paper') {
-        result = 'You lose!'
+        result = 'You lose!';
     }
-    resultDisplay.textContent = result
+    resultDisplay.textContent = result;
 }
